090818214a04c68f68a954cb336fa2a525e0b688
ChangHa001 <dohauzi@gmail.com>
2019-03-19 20:38:25 +0900

    Updates

---
+++ precache-manifest.174919e228d3a6f6fb3a8ebda39f0340.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77a583dbf7f86a63d009",
+    "url": "/react_movie_app/static/js/runtime~main.04e3a753.js"
+  },
+  {
+    "revision": "75388bb735147591f891",
+    "url": "/react_movie_app/static/js/main.176f77ee.chunk.js"
+  },
+  {
+    "revision": "79876ddfb540893b229e",
+    "url": "/react_movie_app/static/js/2.28db7df0.chunk.js"
+  },
+  {
+    "revision": "75388bb735147591f891",
+    "url": "/react_movie_app/static/css/main.cc803961.chunk.css"
+  },
+  {
+    "revision": "a0e738ec01bb1da3cc95c3d11b9b2b7c",
+    "url": "/react_movie_app/index.html"
+  }
+];
